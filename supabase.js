bfcd681b76d7079a6806a084fb18626509aa6634
daxtonhub <daxtonhub6@gmail.com>
2026-06-13 21:12:06 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // Replace these with your actual Supabase project credentials
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://retndnwgbglbrzfdbxyn.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJldG5kbndnYmdsYnJ6ZmRieHluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNTUwMjksImV4cCI6MjA5NjkzMTAyOX0.kLGrttUz_pKOZycTmNEDRb5TDoPKMLAnVHSFUu5A_gI";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
